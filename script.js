e28c954147031718423a396bb57fab0f8eb54613
mohit-200 <82448012+mohit-200@users.noreply.github.com>
2021-07-04 16:55:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,8 +27,10 @@ function getRandomWord() {
 }
 
 function addWordToDOM() {
+  document.getElementById("word").style.display="block";
   randomWord = getRandomWord();
   word.innerHTML = randomWord;
+  setTimeout(function(){ document.getElementById("word").style.display="none"; }, 3000);
 }
 addWordToDOM();
 
